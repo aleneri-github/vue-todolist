fe6c3066621570077c1d1eec051a7b4b78823201
Alessandro <aleneri242@gmail.com>
2020-12-10 12:20:16 +0100

    Aggiunto campo input e pulsante

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var root = new Vue({
 el: '#root',
 data: {
-message: "TO DO LIST",
+// - creare una lista di cose da fare e mostrarla in pagina con Vue;
 todolist: [
   "Fare la spesa",
   "Comprare il pane",
